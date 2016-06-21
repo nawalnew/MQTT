08ed29113030f460702cacf4f45fd863cb377b14
Sergej Nawalnew <hello@nawalnew.com>
2016-06-21 21:29:39 +0200

    passing variable

--- controller.js
+++ controller.js
@@ -1,21 +1,46 @@
-//
+// load the things we need
 var express = require('express');
 var app = express();
-var path    = require("path");
 
-//MQTT
-const mqtt = require('mqtt')
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  // res.send('Hello World!');
-  res.sendFile(path.join(__dirname, '/', 'index.html'));
+var temp = 50;  //here you assign temp variable with needed value
+// index page
+app.get('/', function(req, res) {
+
+    var total = temp+10;
+    res.render('pages/index', { //render the index.ejs
+    temp: temp,
+    total:total,
+    opened:opened
+  });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(8080);
+console.log('8080 for the win');
+
+/* GET home page. */
+// router.get('/', function(req, res, next) { // route for '/'
+//   var temp = 50;  //here you assign temp variable with needed value
+//   var total = temp+10;
+//   res.render('index', { //render the index.ejs
+//     temp: temp,
+//     total:total
+//   });
+// });
+//
+// var server = app.listen(3000, function() {
+//   var host = server.address().address;
+//   var port = server.address().port;
+//
+//   console.log('Example app listening at http://%s:%s', host, port);
+// });
+
 
 // controller.js
+//MQTT
+const mqtt = require('mqtt')
 
 const client = mqtt.connect('mqtt://broker.hivemq.com')
 
@@ -50,12 +75,15 @@ function handleGarageState (message) {
   console.log('garage state update to %s', message)
 }
 
+var opened = 1;
+
 function openGarageDoor () {
   // can only open door if we're connected to mqtt and door isn't already open
   if (connected && garageState !== 'open') {
     // Ask the door to open
     client.publish('garage/open', 'true')
-     testvariable = 'testvariable 2'
+
+    opened = + 5;
 
   }
 }
@@ -65,7 +93,8 @@ function closeGarageDoor () {
   if (connected && garageState !== 'closed') {
     // Ask the door to close
     client.publish('garage/close', 'true')
-    testvariable = 'testvariable 3'
+
+    opened = + 10;
   }
 }
 
@@ -75,10 +104,10 @@ function closeGarageDoor () {
 setTimeout(() => {
   console.log('open door')
   openGarageDoor()
-}, 5000)
+}, 2000)
 
 // simulate closing garage door
 setTimeout(() => {
   console.log('close door')
   closeGarageDoor()
-}, 80000)
+}, 4000)
